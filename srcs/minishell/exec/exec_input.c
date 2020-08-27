3b1023c2b11f8fee9f59794af2f699764d1bf58a
Matthieu LE FAOU <mle-faou@e1r8p14.42.fr>
2020-08-27 16:09:41 +0200

    Update

--- srcs/minishell/exec/exec_input.c
+++ srcs/minishell/exec/exec_input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_input.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vvaucoul <vvaucoul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mle-faou <mle-faou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 18:43:39 by vvaucoul          #+#    #+#             */
-/*   Updated: 2020/08/26 16:06:53 by vvaucoul         ###   ########.fr       */
+/*   Updated: 2020/08/27 16:09:20 by mle-faou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,24 @@ static int		exec_command(char **command, t_mns *mns)
 	struct stat		stat;
 
 	if ((ret = exec_pipes_redirections(command, mns)) > 0)
-		return (ret);
-	if ((ret = exec_builtins(command, mns)) != 1)
-		return (ret);
+		return ((mns->last_return = ret) == -1);
+	if ((ret = exec_builtins(command, mns)) != 2)
+	{
+		printf("ret : %d\n", ret); //todo
+		return ((mns->last_return = ret) == -1);
+	}
 	if (!ft_strchr(command[0], '/'))
 	{
 		if ((ret = exec_system(command, mns)) != 1)
-			return (ret);
+			return ((mns->last_return = ret) == -1);
 	}
 	else if (exec_local_exec(command, mns) != 1)
-		return (ret);
+		return ((mns->last_return = ret) == -1);
 	if (lstat(command[0], &stat) != -1)
 		if (stat.st_mode & S_IFDIR)
-			return (b_cd(mns, command[0]) == -1); // check return
+			return ((mns->last_return = b_cd(mns, command[0])) == -1);
 	display_error_cmd(command[0]);
+	mns->last_return = 127;
 	return (0);
 }
 
@@ -48,7 +52,9 @@ int				exec_input(char **commands, t_mns *mns)
 		if (!(command = quotesplit(commands[i], ' ')))
 			return (-1);
 		// print_table(command, "command");
+		// printf("$? = %d\n", mns->last_return);
 		ret = exec_command(command, mns);
+		// printf("$? = %d\n", mns->last_return);
 		// print_table(commands, "commands");
 		// print_table(command, "command");
 		// printf("ret = %d\n", ret);
